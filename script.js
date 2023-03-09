179f02167fbbccc8813695cd473f23c6275bd0a0
James Pierce <james59222@gmail.com>
2023-03-08 21:03:27 -0600

    started question format and answer

--- script.js
+++ script.js
@@ -1,29 +1,41 @@
-var startBtnEl = document.getElementById("startBtn");
-
-var count=5;
+const startBtnEl = document.getElementById("startBtn");
+const questionElement = document.getElementById
+('questions')
+startBtnEl.addEventListener("click", startQuiz);
+//var count=5;
 function startQuiz () {
-    console.log("hello");
-timer();
+    console.log("Quiz Started");
+    startBtnEl.classList.add()
+    questionContainerElement.classList
+//timer();
 
 }
-function timer() {
+//function timer() {
     setInterval(displayHello, 1000);
     
-function displayHello() {
-  var timerEl = document.getElementById("timer");
-  timerEl.textContent = count
-  count = count - 1
-  if (count <= 0) {
-    console.log("Times Up")
-   stopTimer() 
-  }
+//function displayTimesUp() {
+// var timerEl = document.getElementById("timer");
+//  timerEl.textContent = count
+//  count = count - 1
+//  if (count <= 0) {
+//    console.log("Times Up")
+//   stopTimer() 
+//  }
+//}
+//}
+
+//function stopTimer() {
+//    clearInterval(timer)
+//}
+
+
+function answerSelect(){
+
 }
+const questions = {
+  question: "1 question",
+  answers: [
+    {text: '1', correct: true},
+    {text: '2', correct: false}
+  ]
 }
-
-function stopTimer() {
-    clearInterval(timer)
-}
-
-
-startBtnEl.addEventListener("click", startQuiz);
-
